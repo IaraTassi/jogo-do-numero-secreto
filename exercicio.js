c9e701740ec5f899867ad7f4cc4e1ae9b4c1dea0
Iara Tassi <120215360+IaraTassi@users.noreply.github.com>
2024-01-27 17:16:36 -0300

    Add files via upload
    Subindo os arquivos

---
+++ exercicio.js
@@ -0,0 +1,28 @@
+// 1. Crie uma lista vazia, com o nome listaGenerica.
+let listaGenerica = [];
+
+// 2. Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+let listaDeLinguagemDeProgramacao = [
+  "JavaScript",
+  "C",
+  "C++",
+  "Kottlin",
+  "Python",
+];
+
+// 3 . Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+listaDeLinguagemDeProgramacao.push("Java", "Ruby", "Golang");
+console.log(listaDeLinguagemDeProgramacao);
+
+// 4. Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+let listaDeNomes = ["Paula", "Flávia", "Mariana"];
+console.log(listaDeNomes[0]);
+
+// 5. Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+
+let listaDeNomes2 = ["Camily", "Juliana", "Olivia"];
+console.log(listaDeNomes2[1]);
+
+// 6. Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+
+console.log(listaDeNomes[2]);
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,10 @@
+let listaDeLinguagemDeProgramacao = [
+  "JavaScript",
+  "C",
+  "C++",
+  "Kottlin",
+  "Python",
+];
+
+// 3 . Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+listaDeLinguagemDeProgramacao.push("Java", "Ruby", "Golang");
